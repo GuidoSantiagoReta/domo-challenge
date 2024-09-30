af42751a599740b867923993a82adbee37dceee4
GuidoSantiagoReta <guidosantiagoreta@gmail.com>
2024-09-30 15:24:06 -0300

    - Se agrega position fixed para el  nav active.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.navbar-links');
-    const body = document.querySelector('body');
-    
+
+
     burger.addEventListener('click', () => {
         // Toggle nav visibility
         nav.classList.toggle('nav-active');
@@ -10,12 +10,7 @@ const navSlide = () => {
         // Burger animation
         burger.classList.toggle('toggle');
 
-         // Disable scroll when menu is active
-    if (navbarLinks.classList.contains('nav-active')) {
-        body.classList.add('no-scroll');
-    } else {
-        body.classList.remove('no-scroll');
-    }
+ 
     });
 }
 
