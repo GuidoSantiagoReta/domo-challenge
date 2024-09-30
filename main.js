e04363bbc96cdfd7085b31465b0a9bfed5eb19a0
GuidoSantiagoReta <guidosantiagoreta@gmail.com>
2024-09-30 14:33:23 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navbar-links');
+
+    burger.addEventListener('click', () => {
+        // Toggle nav visibility
+        nav.classList.toggle('nav-active');
+
+        // Burger animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
