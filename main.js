f7ae47910058f9549e8f3f00343adf1b7b1b299c
GuidoSantiagoReta <guidosantiagoreta@gmail.com>
2024-09-30 15:17:29 -0300

    nav scroll

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.navbar-links');
-
+    const body = document.querySelector('body');
+    
     burger.addEventListener('click', () => {
         // Toggle nav visibility
         nav.classList.toggle('nav-active');
 
         // Burger animation
         burger.classList.toggle('toggle');
+
+         // Disable scroll when menu is active
+    if (navbarLinks.classList.contains('nav-active')) {
+        body.classList.add('no-scroll');
+    } else {
+        body.classList.remove('no-scroll');
+    }
     });
 }
 
