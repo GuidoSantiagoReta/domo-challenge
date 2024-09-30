fe51751f4f5cd45e1736790f824433be85d6268e
GuidoSantiagoReta <guidosantiagoreta@gmail.com>
2024-09-30 19:42:39 -0300

    - Se agrega animación de entrada y salida del menú

--- main.js
+++ main.js
@@ -4,17 +4,29 @@ const navSlide = () => {
     const body = document.querySelector('body');
 
     burger.addEventListener('click', () => {
-        // Toggle nav visibility
-        nav.classList.toggle('nav-active');
-
-        // Burger animation
+        // Toggle burger animation
         burger.classList.toggle('toggle');
+
+        if (nav.classList.contains('nav-active')) {
+            // Si el menú está abierto, agregar la animación de salida
+            nav.classList.remove('animate__fadeInDown'); // Quitar la animación de entrada
+            nav.classList.add('animate__animated', 'animate__fadeOutUp'); // Agregar animación de salida
+        } else {
+            // Si el menú está cerrado, agregar la animación de entrada y mostrar el menú
+            nav.classList.add('nav-active', 'animate__animated', 'animate__fadeInDown');
+        }
 
         // Toggle scroll blocking
         body.classList.toggle('no-scroll');
     });
 
-
+    // Escuchar el evento de finalización de la animación
+    nav.addEventListener('animationend', () => {
+        // Si la animación de salida termina, sacar la clase nav-active
+        if (nav.classList.contains('animate__fadeOutUp')) {
+            nav.classList.remove('nav-active', 'animate__fadeOutUp');
+        }
+    });
 }
 
 navSlide();
